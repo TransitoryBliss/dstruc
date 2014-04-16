3bc417a57df3c9797730759e5a8a1f66affa5398
shockwork <info@shockwork.net>
2014-04-16 13:41:25 +0200

    travis ci integ

--- dstruc.js
+++ dstruc.js
@@ -4,20 +4,18 @@ format = require('util').format;
 
 /**
  * Get directory structure recursively (sync).
- * @example
- * 	var ds = require('dstruc');
- *  var structure = ds.sync('/path/to/folder');
- *  console.log(structure); // { files: [], dirs: { content: { files: [], dirs: [Object] } } }
+ * @param {String}  Path                        path
+ * @param {Boolean} extensionAsKey     files is object instead of with extension as key.
  * @todo
  * 	Add async version
  */
-var sync = function (dir) {
+var sync = function (path, extensionAsKey) {
 	var files, structure;
-	files = fs.readdirSync(dir);
+	files = fs.readdirSync(path);
 	structure = {files: [], dirs: {}};
 	files.forEach(function (file, index) {
 		if (file[0] !== '.') {
-			var filepath = format('%s/%s', dir, file);
+			var filepath = format('%s/%s', path, file);
 			var stat = fs.statSync(filepath);
 			if (stat.isDirectory()) {
 				structure['dirs'][file] = sync(filepath); // directory, parse it
