a9178ac74026675c091dcfe77b059a0e3f051350
shockwork <info@shockwork.net>
2014-09-25 18:43:10 +0200

    cleanup

--- dstruc.js
+++ dstruc.js
@@ -1,52 +1,58 @@
-var fs, format;
-fs = require('fs'),
-format = require('util').format;
+var fs = require('fs')
+  , format = require('util').format
+  , mixin = require('utils-merge');
 
 /**
- * Get directory structure recursively (sync).
- * @param {String}  path          path
- * @param [Object] options
-        extensionAsKey: Boolean [false],
-        recursive: boolean [true]
- * @return {Object} structure
- */
+* Get directory structure recursively (sync).
+* @param {String}  path          path
+* @param [Object] options
+	extensionAsKey: boolean [false],
+	recursive: boolean [true]
+* @return {Object} structure
+*/
 var sync = function (path, options) {
-  var files, structure, recursive, extensionAsKey;
+ 	var files, defaults, structure, recursive, extensionAsKey;
 
-  options = options || {};
+ 	defaults = { extensionAsKey: false, recursive: true };
 
-  recursive = (options.recursive === false) ? false : true;
-  extensionAsKey = (options.extensionAsKey === true) ? true : false;
+ 	options = mixin(defaults, options);
 
-  files = fs.readdirSync(path);
-  structure = {};
-  structure.files = (extensionAsKey) ? {} : [];
-  structure.dirs = (recursive) ? {} : [];
+ 	recursive = options.recursive;
+ 	extensionAsKey = options.extensionAsKey;
 
-  files.forEach(function (file, index) {
-    var stat, filepath;
-    if (file[0] !== '.') {
+ 	structure = {};
+ 	structure.files = (extensionAsKey) ? {} : [];
+ 	structure.dirs = (recursive) ? {} : [];
 
-    filepath = format('%s/%s', path, file);
-    stat = fs.statSync(filepath);
-    if (stat.isDirectory()) {
-      if (recursive)
-        structure.dirs[file] = sync(filepath); // directory, parse it
-      else
-        structure.dirs.push(file);
-    } else {
-      if (extensionAsKey) {
-        var ext = file.split('.')
-        ext = ext[ext.length-1];
-        if (!structure.files[ext]) structure.files[ext] = [];
-          structure.files[ext].push(file);
-        } else {
-          structure.files.push(file); // file, push it to array
-        }
-      }
-    }
-  });
-  return structure;
+ 	// Loop through all files specified by @path
+ 	fs.readdirSync(path).forEach(function (file, index) {
+ 		var filepath;
+ 		// Do not parse '.'
+ 		if (file[0] !== '.') {
+ 			// the current file´s path (interpolation for ease of use..)
+ 			filepath = format('%s/%s', path, file);
+ 			// Use fs.stat to determine if directory or file
+ 			if (fs.statSync(filepath).isDirectory()) {
+ 				if (recursive) { // if option.recursive === true, recursively call self to parse directory
+					structure.dirs[file] = sync(filepath);
+				} else { // option.recursive === false, just append name to dirs array
+					structure.dirs.push(file);
+				}
+			} else {
+				// it's a file if in here...
+				if (extensionAsKey) { // if options.extensionAsKey === true, add to object
+					var ext = file.split('.');
+					ext = ext[ext.length-1]; // get last value of array (the files extension)
+					structure.files[ext] = structure.files[ext] || [];
+					structure.files[ext].push(file);
+				} else { // if options.extensionAsKey === false, just push it in to array..
+					structure.files.push(file);
+				}
+			}
+		}
+	});
+
+ 	return structure;
 }
 
 exports.sync = sync;
